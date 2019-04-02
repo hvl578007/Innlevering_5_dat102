4543cad9300bf80d83cc383160afaf0452ec4697
Stian Grønås <42640705+hvl578007@users.noreply.github.com>
2019-04-02 13:13:23 +0200

    Initial commit

---
+++ Oving_6/src/no/hvl/dat102/Oppgave_2_klient.java
@@ -0,0 +1,53 @@
+﻿package no.hvl.dat102;
+
+import no.hvl.dat102.bst.*;
+import java.util.*;
+/**
+ * Oppgave_2_klient
+ */
+public class Oppgave_2_klient {
+
+    public static void main(String[] args) {
+        Random tilfeldig = new Random();
+
+        int talPaaTre = 100;
+        int talPaaNoder = 8192;
+
+        KjedetBinaerSokeTre<Integer>[] bstTab = (KjedetBinaerSokeTre<Integer>[]) new KjedetBinaerSokeTre[talPaaTre];
+
+        for (int i = 0; i < talPaaTre; i++) {
+            bstTab[i] = new KjedetBinaerSokeTre<Integer>();
+
+            for (int x = 0; x < talPaaNoder; x++) {
+                Integer tal = tilfeldig.nextInt();
+                bstTab[i].leggTil(tal);
+            }
+        }
+
+        System.out.println("Tal på noder: " + talPaaNoder);
+        System.out.println("Min teoretisk høgde: " + minTeoretiskHogdeBST(talPaaNoder));
+        System.out.println("Maks teoretisk høgde: " + (talPaaNoder-1));
+
+        int minHogde = talPaaNoder;
+        int maksHogde = 0;
+        int totHogde = 0;
+        for (int i = 0; i < talPaaTre; i++) {
+            int hogde = bstTab[i].hogde();
+            if(hogde < minHogde) {
+                minHogde = hogde;
+            }
+            if(hogde > maksHogde) { //else if?
+                maksHogde = hogde;
+            }
+            totHogde += hogde;
+        }
+
+        System.out.println("Min høgde: " + minHogde + "\nMaks høgde: " + maksHogde);
+        double gjnSnitthogde = (1.0*totHogde) / talPaaTre;
+        System.out.println("Gjennomsnittleg høgde: " + gjnSnitthogde + ", rundaAv = " + Math.round(gjnSnitthogde));
+    }
+
+    public static int minTeoretiskHogdeBST(int n) {
+        return (int)(Math.ceil(Math.log(n)/Math.log(2)));
+    }
+}
---
+++ Oving_6/src/no/hvl/dat102/bst/BinaerTreNode.java
@@ -0,0 +1,65 @@
+﻿package no.hvl.dat102.bst;
+//*******************************************************************
+//  BinorTreNode.java               
+//
+//  Representerer en node i et binort tre.
+//*******************************************************************
+
+public class BinaerTreNode<T> {
+   private T element;
+   private BinaerTreNode<T> venstre, hoyre;
+
+   
+   
+   /*****************************************************************
+     Oppretter et nytt tre med spesifisert data.
+   *****************************************************************/
+   BinaerTreNode (T el) {
+      element   = el;
+      venstre   = null;
+      hoyre     = null;
+   }
+         
+   /******************************************************************/
+   /*   Hent- og settmetoder
+   /******************************************************************/
+   
+    
+ public BinaerTreNode<T> getVenstre(){
+  return venstre;
+ }
+ 
+ public BinaerTreNode<T> getHoyre(){
+  return hoyre;
+ }
+ 
+ public void setVenstre(BinaerTreNode<T> ny){
+  venstre = ny;
+ }
+ 
+ public void setHoyre(BinaerTreNode<T> ny){
+  hoyre = ny;
+ }
+ 
+ public T getElement(){
+  return element;
+ }
+ public void setElement(T el){
+  element = el;
+ }
+ 
+   /*****************************************************************
+     Returnerer antall barn ulik null.     
+   *****************************************************************/
+   public int antallBarn() {
+      int barn = 0;
+
+      if (venstre != null)
+         barn = 1 + venstre.antallBarn();
+
+      if (hoyre != null)
+         barn = barn + 1 + hoyre.antallBarn();
+
+      return barn;
+   }
+}
